df73cbc23853d665c937726e3a691e48c54ff1c1
Doug Perez <56580725+dougwperez@users.noreply.github.com>
2020-02-12 16:24:35 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,57 @@
+import Compositor from './Compositor.js';
+import {loadLevel} from './loaders.js';
+import {loadMarioSprite, loadBackgroundSprites} from './sprites.js';
+import {createBackgroundLayer} from './layers.js';
+
+
+
+
+
+
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d')
+
+
+
+
+function createSpriteLayer(sprite, pos) {
+	return function drawSpriteLayer(context) {
+		for(let i=0; i < 20; ++i) {
+		sprite.draw('idle', context, pos.x + i * 8, pos.y);
+	}
+	};
+}
+
+Promise.all ([
+	loadMarioSprite(),
+	loadBackgroundSprites(),
+	loadLevel('1-1'),
+	])
+	.then(([marioSprite, backgroundsprites, level,]) => {
+		const comp = new Compositor();
+
+		const backgroundLayer= createBackgroundLayer(level.backgrounds, backgroundsprites);
+		comp.layers.push(backgroundLayer);
+
+
+	  
+
+		const pos = {
+			x: 0,
+			y: 0,
+		};
+
+		const spriteLayer = createSpriteLayer(marioSprite, pos);
+		comp.layers.push(spriteLayer);
+
+		function update() {
+			comp.draw(context);
+			pos.x +=2;
+			pos.y +=2;
+			requestAnimationFrame(update);
+		}
+
+		update();
+
+		
+	});	
