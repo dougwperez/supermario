482314155c7be0a1f941fbd00241666752f6bbd9
Doug Perez <56580725+dougwperez@users.noreply.github.com>
2020-03-19 22:41:06 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,53 +1,75 @@
 import Camera from './Camera.js';
+import Entity from './Entity.js';
+import PlayerController from './traits/PlayerController.js';
 import Timer from './Timer.js';
-import {loadLevel} from './loaders/level.js';
+import {createLevelLoader} from './loaders/level.js';
+import {loadFont} from './loaders/font.js';
 import {loadEntities} from './entities.js';
 import {setupKeyboard} from './input.js';
-import {createCollisionLayer} from './layers.js';
+import {createCollisionLayer} from './layers/collision.js';
+import {createDashboardLayer} from './layers/dashboard.js';
 
-const canvas = document.getElementById('screen');
-const context = canvas.getContext('2d');
+function createPlayerEnv(playerEntity) {
+    const playerEnv = new Entity();
+    const playerControl = new PlayerController();
+    playerControl.checkpoint.set(64, 64);
+    playerControl.setPlayer(playerEntity);
+    playerEnv.addTrait(playerControl);
+    return playerEnv;
+}
 
-Promise.all([
-    loadEntities(),
-    loadLevel('1-1'),
-])
-.then(([entity, level]) => {
-    console.log(entity);
+async function main(canvas) {
+    const context = canvas.getContext('2d');
+    const audioContext = new AudioContext();
+
+    const [entityFactory, font] = await Promise.all([
+        loadEntities(audioContext),
+        loadFont(),
+    ]);
+
+
+    const loadLevel = await createLevelLoader(entityFactory);
+
+    const level = await loadLevel('1-1');
 
     const camera = new Camera();
-    window.camera = camera;
 
-    const mario = entity.mario();
-    mario.pos.set(64, 64);
+    const mario = entityFactory.mario();
 
-    const goomba = entity.goomba();
-    goomba.pos.x = 220;
-    level.entities.add(goomba);
+    const playerEnv = createPlayerEnv(mario);
+    level.entities.add(playerEnv);
 
-
-    const koopa = entity.koopa();
-    koopa.pos.x = 260;
-    level.entities.add(koopa);
-
-
-    level.entities.add(mario);
 
     level.comp.layers.push(createCollisionLayer(level));
+    level.comp.layers.push(createDashboardLayer(font, playerEnv));
 
     const input = setupKeyboard(mario);
     input.listenTo(window);
 
+    const gameContext = {
+        audioContext,
+        deltaTime: null,
+    };
+
     const timer = new Timer(1/60);
     timer.update = function update(deltaTime) {
-        level.update(deltaTime);
+        gameContext.deltaTime = deltaTime;
+        level.update(gameContext);
 
-        if (mario.pos.x > 100) {
-            camera.pos.x = mario.pos.x - 100;
-        }
+        camera.pos.x = Math.max(0, mario.pos.x - 100);
 
         level.comp.draw(context, camera);
     }
 
     timer.start();
-});
+}
+
+const canvas = document.getElementById('screen');
+
+const start = () => {
+    window.removeEventListener('click', start);
+    main(canvas);
+};
+
+window.addEventListener('click', start);
+
